a486ed1ca57c7ba3b9ae9405f9dd94358cc984db
SanketMole <sanketmole78@gmail.com>
2025-03-10 20:26:07 +0530

    Display images from DB on the page

--- newDjango/SSM/views.py
+++ newDjango/SSM/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import ChaiVarity
 
 # Create your views here.
 def all_SSM(request):
-    return render(request, 'SSM/all_SSM.html')
+    chais = ChaiVarity.objects.all()
+    return render(request, 'SSM/all_SSM.html', {'chais': chais})
