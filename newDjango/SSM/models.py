d8136d8339e5a4b79a59c8528bbaa0c34feb9700
SanketMole <sanketmole78@gmail.com>
2025-03-14 23:52:05 +0530

    One to many Django relationship model

--- newDjango/SSM/models.py
+++ newDjango/SSM/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -19,3 +20,15 @@ class ChaiVarity(models.Model):
     
     def __str__(self):
         return self.name
+    
+# One to many 
+
+class ChaiReview(models.Model):
+    chai = models.ForeignKey(ChaiVarity, on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    comment = models.TextField()
+    date_added = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'{self.user.username} review for {self.chai.name}'
