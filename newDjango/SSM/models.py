83b0a82f64fccea677c6a90d53af6fb40392e6f3
SanketMole <sanketmole78@gmail.com>
2025-03-17 15:07:04 +0530

    Revert "Registered new models (ChaiVarietyAdmin, StoreAdmin, ChaiCertificateAdmin)"
    This reverts commit df6ea46088b374c630cebff70e5c7c752a1af33e.

--- newDjango/SSM/models.py
+++ newDjango/SSM/models.py
@@ -32,25 +32,3 @@ class ChaiReview(models.Model):
 
     def __str__(self):
         return f'{self.user.username} review for {self.chai.name}'
-    
-# Many to many
-
-class Store(models.Model):
-    name = models.CharField(max_length=100)
-    location = models.CharField(max_length=100)
-    chai_varieties = models.ManyToManyField(ChaiVarity, related_name='stores')
-
-    def __str__(self):
-        return self.name
-    
-# One to one
-
-class ChaiCertificate(models.Model):
-    chai = models.OneToOneField(ChaiVarity, on_delete=models.CASCADE, related_name='certificate')
-    certificate_number = models.CharField(max_length=100)
-    issued_date = models.DateTimeField(default=timezone.now)
-    valid_until = models.DateTimeField()
-
-    def __str__(self):
-        return f'Certificate for {self.name.chai}'
-     
